f1b412edfcff72f0cbe9ad9f09cf2a34226ddc02
Patrik <pjurak3@gmail.com>
2022-04-21 12:48:28 +0200

    Login screen (new design)

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import {gsap} from "gsap";
 import "./signin.css";
 
 function LoginForm({ Login, error }) {
@@ -10,40 +11,50 @@ function LoginForm({ Login, error }) {
     Login(details);
   };
 
+  const boxRef = useRef();
+
+  useEffect(() => {
+    gsap.to(boxRef.current, { x: "0" }, {x: "-1000"});
+  })
+
   return (
     <div class="flex-container">
       <div class="content-container">
         <div class="form-container">
           <form onSubmit={submitHandler}>
             <div class="form-inner">
-              <h2>Login</h2> <br />
+              <h2 id="login-greetings" ref={boxRef}>Hey, Welcome Back!</h2> <br />
               {/* ERROR! */}
               <div class="subtitle">
-                <label htmlFor="email">Korisničko ime:</label> <br />
                 <input
                   type="email"
                   name="email"
                   id="email"
+                  placeholder="Email id"
+                  autoComplete="off"
                   onChange={(e) =>
                     setDetails({ ...details, email: e.target.value })
                   }
                   value={details.email}
                 />
+                
               </div>
               <div class="subtitle">
-                <label htmlFor="password">Lozinka:</label> <br />
                 <input
                   type="password"
                   name="password"
                   id="password"
+                  placeholder="Password"
+                  autoComplete="off"
                   onChange={(e) =>
                     setDetails({ ...details, password: e.target.value })
                   }
                   value={details.password}
                 />
-              </div>{" "}
-              <br />
-              <input type="submit" value="LOGIN" class="submit-btn" />
+              </div>
+                  <a href="/" className="forgot-pass">Forgot password?</a>
+                  <button type="submit" className="submit-btn">Login</button>
+              {/* <input type="submit" value="LOGIN" class="submit-btn" /> */}
             </div>
           </form>
         </div>
